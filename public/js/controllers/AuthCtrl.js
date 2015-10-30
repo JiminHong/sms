c7ac49b9013306a142d86497e425c27bd2e3aa9d
Jimin Hong <jhong.dev@gmail.com>
2015-10-30 16:01:13 -0400

    testing register | not yet

--- public/js/controllers/AuthCtrl.js
+++ public/js/controllers/AuthCtrl.js
@@ -1,22 +1,47 @@
 
 var myapp = angular.module('myapp');
 
-myapp.factory("Auth", function ($firebaseAuth){
-	var ref = new Firebase("https://amber-fire-1000.firebaseio.com/");
-	return $firebaseAuth(ref);
-});
+// myapp.factory("Auth", function ($firebaseAuth){
+// 	var ref = new Firebase("https://amber-fire-1000.firebaseio.com/");
+// 	return $firebaseAuth(ref);
+// });
 
-myapp.controller("AuthCtrl", ["$scope", "Auth", function ($scope, Auth){
+myapp.controller('AuthCtrl', ["$scope", "$firebaseAuth", "$firebaseObject", "$location", "$routeParams",
+	function ($scope, $firebaseAuth, $firebaseObject, $location, $routeParams){
+		var ref = new Firebase("https://amber-fire-1000.firebaseio.com/");
+		$scope.authObj = $firebaseAuth(ref);
 
-		Auth.$onAuth = function(authData){
-			$scope.authData = authData;
-		}
+		$scope.register = function() {
+			console.log("$scope.authObj :::::", $scope.registerUser);
+
+			$scope.authObj.$createUser({
+				email: $scope.registerUser.email,
+				password: $scope.registerUser.password
+			})
+			.then(function(userData){
+				console.log(userData.uid);
+
+				return $scope.authObj.$authWithPassword({
+					email: $scope.registerUser.email,
+					password: $scope.registerUser.password
+				});
+			})
+			.then(function(authData) {
+				console.log("Logged in as ", authData.uid)
+			})
+			.catch(function(error) {
+				console.error(error);
+			})
+
+        };
+
+
 
 		$scope.login = function(){
 			Auth.$authWithOAuthPopup("facebook")
 			.then(function(authData){
 				$scope.authData = authData;
-				console.log("when $onAuth :::: ",authData);
+				console.log("authData :::: ",authData);
 			})
 			.catch(function(error) {
 				console.log(error);
@@ -25,6 +50,8 @@ myapp.controller("AuthCtrl", ["$scope", "Auth", function ($scope, Auth){
 
 		$scope.logout = function() {
 			Auth.$unauth();
+			console.log("Logout");
+			$location.path('/');
 		}
 
 		console.log("authCtrl is fired");
